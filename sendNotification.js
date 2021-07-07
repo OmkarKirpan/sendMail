6ea2a53c29beafb2a58337973b1689eb65219615
Omkar Kirpan <okirpan@gmail.com>
2021-07-07 14:56:33 +0530

    fix: sendNotification Url

--- sendNotification.js
+++ sendNotification.js
@@ -1,17 +1,14 @@
 const fetch = require("node-fetch");
 
 const sendNotification = (payload) => {
-  fetch(
-    "https://maker.ifttt.com/trigger/statusReport/with/key/ju2lNgJEV-5Kq-nlcOjFThVXswTAisUEhM0HeYd-fpU",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        value1: payload,
-      }),
-    }
-  );
+  fetch(process.env.NOTIFY_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      value1: payload,
+    }),
+  });
 };
 exports.sendNotification = sendNotification;
